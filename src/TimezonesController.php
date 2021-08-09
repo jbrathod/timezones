019afd25145f0751bac6d8f66cb0c360e70a4d9c
jigneshsinh <jigneshpatterndrive@gmail.com>
2021-08-09 19:18:54 +0530

    first commit

---
+++ src/TimezonesController.php
@@ -0,0 +1,13 @@
+<?php
+namespace Jbrathod\Timezones;
+
+use Carbon\Carbon;
+use App\Http\Controllers\Controller;
+
+class TimezonesController extends Controller
+{
+    public function index($timezone)
+    {
+        echo Carbon::now($timezone)->toDateTimeString();
+    }
+}
---
+++ src/TimezonesServiceProvider.php
@@ -0,0 +1,27 @@
+<?php 
+namespace Jbrathod\Timezones;
+
+use Illuminate\Support\ServiceProvider;
+
+class TimezonesServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        include __DIR__.'/routes.php';
+    }
+}
